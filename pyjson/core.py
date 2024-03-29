7bebc07c91bda1739b2bae17700482d1c2c8061f
Simon Nganga <theedushbag@gmail.com>
2024-03-29 23:25:50 +0300

    Type bugs and appreciation
    This is probably my last commit in this repo as the project is
    officially done. I'm having a hard time understanding python types. I
    mean if we have 3 classes A, B and C where B inherits from A and C from
    B doesn't that mean that all c's are A's and B's. C will inherit all
    properties and methods of A and B hence C is an A and a B. This also
    applies to B and A, all B's are A's. But for whatever reason my static
    analysis is flagging errors when I pass a C in place of B, do they
    expect me to pass an A all should it always be a B. This has to do with
    covariance and contravariance or it is just a bug in the initializer.
    More tests of this will be done in the code repo. For now, GoodBye.

--- pyjson/core.py
+++ pyjson/core.py
@@ -4,22 +4,22 @@ BaseTypes = dict | int | float | str | list | None | bool
 
 
 class Visitor(ty.Protocol):
-    def visit_object(self, object: "Value") -> ty.Any:
+    def visit_object(self, object) -> ty.Any:
         ...
 
-    def visit_array(self, array: "Value") -> ty.Any:
+    def visit_array(self, array) -> ty.Any:
         ...
 
-    def visit_number(self, number: "Value") -> ty.Any:
+    def visit_number(self, number) -> ty.Any:
         ...
 
-    def visit_string(self, string: "Value") -> ty.Any:
+    def visit_string(self, string) -> ty.Any:
         ...
 
-    def visit_boolean(self, boolean: "Value") -> ty.Any:
+    def visit_boolean(self, boolean) -> ty.Any:
         ...
 
-    def visit_null(self, null: "Value") -> ty.Any:
+    def visit_null(self, null) -> ty.Any:
         ...
 
 
